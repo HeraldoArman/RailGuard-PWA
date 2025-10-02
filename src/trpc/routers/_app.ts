ff487e514e66a2a3621d7c6b439291ecce34c4e5
HeraldoArman <heraldoarman7@gmail.com>
2025-10-02 18:43:01 +0700

    fix: trpc

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,11 +1,12 @@
 // import { z } from "zod";
 import { createTRPCRouter } from "../init";
-import { gerbongRouter } from "@/modules/dashboard/server/procedures";
+import { gerbongRouter, kasusRouter } from "@/modules/dashboard/server/procedures";
 
 export const appRouter = createTRPCRouter({
 //   agents: agentsRouter,
 //   meetings: meetingsRouter,
     gerbong: gerbongRouter,
+    kasus: kasusRouter,
 });
 // export type definition of API
 export type AppRouter = typeof appRouter;
