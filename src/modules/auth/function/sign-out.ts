67973770d3847f1821ccedb3d691315a21033543
HeraldoArman <heraldoarman7@gmail.com>
2025-10-02 12:54:59 +0700

    setup dependency

---
+++ src/modules/auth/function/sign-out.ts
@@ -0,0 +1,15 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+import { useRouter } from "next/navigation";
+import { authClient } from "@/lib/auth-client";
+
+export const onLogout = () => {
+  const router = useRouter();
+
+  authClient.signOut({
+    fetchOptions: {
+      onSuccess: () => {
+        router.push("/sign-in");
+      },
+    },
+  });
+};
---
+++ src/trpc/routers/_app.ts
@@ -0,0 +1,10 @@
+import { z } from "zod";
+import { createTRPCRouter } from "../init";
+
+
+export const appRouter = createTRPCRouter({
+//   agents: agentsRouter,
+//   meetings: meetingsRouter,
+});
+// export type definition of API
+export type AppRouter = typeof appRouter;
